85d1920c62421356ccb2a4008b4952b459477b90
Alexandr Savinov <savinov@conceptoriented.org>
2017-08-02 16:56:38 +0200

    commenting evaluator interfaces

--- src/test/java/org/conceptoriented/sc/core/EvaluatorB.java
+++ src/test/java/org/conceptoriented/sc/core/EvaluatorB.java
@@ -9,38 +9,10 @@ import org.conceptoriented.sc.core.EvaluatorBase;
 import org.conceptoriented.sc.core.Range;
 
 /**
- * A user-defined evaluator class implements a function which returns one value given some input values.
- * A user-defined evaluator knows input columns by name by declaring its dependence on these input columns (and their types).
- * Therefore, each such evaluator class is intended for certain input column names only because these names are encoded into it as dependencies.
- * Thus each class cannot be reused for other columns. It is a drawback because we cannot develop generic functions which could be applied to arbitrary columns.
- * Note that the output column name is not encoded into the class.
- * One possible solution to this problem is to use additional specification along with each user defined evaluator. This specification provides dependencies and essentially binds one instance to specific columns by name.
- * In this case, a user defined evaluator implements a generic function for certain input column types but it can be applied to different columns in a schema and this information is provided in the evaluator descriptor.
- * A descriptor (dependencies) could be hard-coded or provided as a separate file, e.g., json file with the same name as the class.
- * Alternatively, dependencies (descriptor) could be provided programmatically for each column like setDependencies. An evaluator class then implements its functions in terms of abstract column numbers.
- * A user-defined evaluator is developed as name-independent function which can be bound to different columns by name using a separate descriptor provided to the column.
- * The descriptor is used for dependency management (computing the graph) and for providing direct column data to the evaluator by using integer column identifiers.
- * An advantage is that evaluators are then reusable functions, for example, we could develop a generic evaluator for arithmetic operations.
- * 
- * The next step would be to allow for nested evaluators. This can be implemented by explicitly creating columns but this means materialization of intermediate results.
- * But what if an evaluator wants to reuse another evaluator by applying its function to the current inputs?
- * We need to take into account that any including nested evaluators can access also other records in their input columns.
- * Also, they could require some additional input columns the parent evaluator does not have. The dependency manager has to know about such uses.
- * One approach to declare in the descriptor that this (parent) evaluator will consume data from a child evaluator. 
- * And the child evaluator will consume some columns (by name). 
- * If the parent is consuming some value from a child evaluator then it is equivalent to consuming a value from a column.
- * In this case however, the parent cannot move along the evaluator by reading inputs - it can only compute one single value.
- * Alternatively, a parent could change inputs (if they are values) and evaluate the child many times.
- * Note that evaluator can change integer row identifiers and retrieve arbitrary outputs from a column where a column is viewed as a function.
- * Probably, the same could be done with child evaluators: set inputs and compute output (instead of retrieving it).
- * In fact, we have an alternative: either a function gets input values or it gets inputs of functions with the functions themselves (so that the inputs can be varied).
- * So a (child) evaluator gets either input values or functions with current inputs.
- * 
- * - Evaluator does not deal with and does not rely on column names (of course, it can get them but it is illegal)
- * - Evaluator uses column objects as (primitive or material) functions which return output for certain input (maybe we need an interface for that purpose which is similar to generic functions/evaluator concept)
- * - Since evaluator uses functions, it has to be able to get validity ranges for them by retrieving various ranges like new, old, or all (so maybe it could be part of generic function/evaluator interface)
- * - Evaluator can vary inputs of the functions it needs, for example, to compute moving average or to shift them. So it can generate new (local) iterations and loops similar to an external driver like the central evaluator which provides the main loop.  
- * - Generic evaluator/function has two alternative interpretations: computing single output for certain input values provided from outside, and computing a range of outputs for a range of inputs provided from outside. 
+ * The evaluator class implements a user-defined function.
+ * An instance of this class gets a list of column references which are resolved from the meta-information provided in the descriptor.
+ * It uses then these columns to read or write the necessary data using inputs. 
+ * The first input id is provided as a parameter of the evaluate method.  
  */
 public class EvaluatorB extends EvaluatorBase {
 
